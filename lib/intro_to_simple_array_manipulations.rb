9fea411b782ef4a8db32f6b2c2a0095a823246c4
Adri Torres <adriana.amaral@msn.com>
2020-10-27 19:32:49 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push (array, string)
+  array.push(string)
+end
+
+def using_unshift (array, string)
+  array.unshift(string)
+end
+
+def using_pop(continents)
+  
+  continents.pop
+end
+
+def pop_with_args(dog_breeds)
+ 
+  dog_breeds.pop(2)
+end
+
+def using_shift(array)
+  
+array.shift
+end
+
+def shift_with_args(array)
+  
+ array.shift(2)
+end
+
